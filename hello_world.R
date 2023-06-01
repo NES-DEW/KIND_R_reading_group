4dcdbfd79168cf6dfcf3ca847090fc4c11f50400
Brendan Clarke <brendan.clarke2@nhs.scot>
2023-06-01 14:50:00 +0100

    added base packages

--- hello_world.R
+++ hello_world.R
@@ -1,1 +1,9 @@
 # hello_world.R
+
+# suggested packages for R4DS2e
+install.packages(
+  c("tidyverse", "arrow", "babynames", "curl", "duckdb", "gapminder", 
+    "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman", 
+    "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", 
+    "repurrrsive", "tidymodels", "writexl")
+)
